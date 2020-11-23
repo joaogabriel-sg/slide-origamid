a9d9d7d79add6e37d8a4aff513e0b259225d4a61
João Gabriel <joaogabrielsg.00@gmail.com>
2020-11-23 14:15:24 -0300

    Configs and index for slides

--- js/slide.js
+++ js/slide.js
@@ -20,7 +20,7 @@ export default class Slide {
         if (event.type === 'mousedown') {
             event.preventDefault();
             this.dist.startX = event.clientX;
-            movetype = 'mousedown';
+            movetype = 'mousemove';
         } else {
             this.dist.startX = event.changedTouches[0].clientX;
             movetype = 'touchmove';
@@ -53,9 +53,39 @@ export default class Slide {
         this.onEnd = this.onEnd.bind(this);
     }
 
+    // Slides Config
+    slidePosition(slide) {
+        const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+        return -(slide.offsetLeft - margin);
+    }
+
+    slidesConfig() {
+        this.slideArray = [...this.slide.children].map((element) => {
+            const position = this.slidePosition(element);
+            return { element, position }
+        });
+    }
+
+    slideIndexNav(index) {
+        const last = this.slideArray.length - 1
+        this.index = {
+            prev: index ? index - 1 : undefined,
+            active: index,
+            next: index === last ? undefined : index + 1,
+        }
+    }
+
+    changeSlide(index) {
+        const activeSlide = this.slideArray[index]
+        this.moveSlide(activeSlide.position);
+        this.slideIndexNav(index);
+        this.dist.finalPosition = activeSlide.position;
+    }
+
     init() {
         this.bindEvents();
         this.addSlideEvents();
+        this.slidesConfig();
         return this;
     }
 }
